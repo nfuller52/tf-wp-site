11d7cab1c2b41a01ba26943f178a1328a4df4124
Nick Fuller <nfuller52@gmail.com>
2015-08-29 09:04:41 -0700

    Introduce redis caching

---
+++ wp-config-development.php
@@ -0,0 +1,23 @@
+<?php
+
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', true );
+define( 'SAVEQUERIES', true );
+
+define('DB_NAME', 'tasukuforce_development');
+define('DB_USER', 'tasukuenforcer');
+define('DB_PASSWORD', 'Kur0sak1');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', 'utf8_general_ci');
+
+define('AUTH_KEY',          'Z98CtVy$kH}Lp^]J1H4=+]mY!q!D/)GPC9IL4(n4z~SkcZ%10a68f!nF1mi/tKHc');
+define('SECURE_AUTH_KEY',   '^C.R:RQ!0rP7puW!q~:OB*9Z1>k41+pl.q/Y@Df]n-.2O-d3q+ifWt~j!R|(|[Oy');
+define('LOGGED_IN_KEY',     '{*Q?0FdJnYJh7Iznqz(xC^b4uP32Qv%URBGxl#q--;JG)Sp+Q-FIU;E+y8>hP+,b');
+define('NONCE_KEY',         'h8C(yX2njlG`F2ywp1vhqn~hopd-4?>=Q^h^:B9-G+XOf+fV<E:muuHam71|760]');
+define('AUTH_SALT',         '~W!dw@F+ yZI6:*W&iro8RcA)+|~|Jae*fr Mpi>qI0g|kPHg+@g|k,lGYKDUPs0');
+define('SECURE_AUTH_SALT',  'eYVc5jBY5nwacsl~+d8q0A^/E^kg*}vOlB?W]dW^+ydQ6b3/aU<W.^+0+:S;hxAn');
+define('LOGGED_IN_SALT',    '/axgB*Ep>%I{[`71zEHpu)>}Jci0LN.GFBwH[WV6kCdK<c%+^;/ e|;OYtSk&q_w');
+define('NONCE_SALT',        'vuHw_6-w+[#}O? -A?o*c(n&?vMDcrjiNw~-|P|q+rp= W/{,EXSr|.Y;$V)=|6W');
